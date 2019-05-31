36a511c30df6a79163ecee02ac3a5ba01f034e72
Maikel Chandika <mchandika@bbmtek.com>
2019-05-31 11:04:42 +0700

    fix error build

--- pekbot-server/src/main/kotlin/com/kmklabs/pekbotserver/PekbotServerApplication.kt
+++ pekbot-server/src/main/kotlin/com/kmklabs/pekbotserver/PekbotServerApplication.kt
@@ -42,8 +42,6 @@ class PekbotController {
 	@GetMapping("/health")
 	fun health() : ResponseEntity<String> {
 
-		logger.info("\n ${mapper.writerWithDefaultPrettyPrinter().writeValueAsString(httpRequest)}")
-
 		return ResponseEntity.ok("UP")
 	}
 }
